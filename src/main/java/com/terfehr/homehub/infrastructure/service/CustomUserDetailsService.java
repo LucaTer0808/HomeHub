944b48a7561fd6e6e6d586fe6bf0c4481ff00e7e
Luca Nicolas Terfehr <luca.terfehr@outlook.de>
2025-09-28 18:25:43 +0200

    #11 - nitpicking

--- src/main/java/com/terfehr/homehub/infrastructure/service/CustomUserDetailsService.java
+++ src/main/java/com/terfehr/homehub/infrastructure/service/CustomUserDetailsService.java
@@ -26,6 +26,5 @@ public class CustomUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
-
     }
 }
